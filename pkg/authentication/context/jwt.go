ca66e5c0b5dd087a2e17fb7be5df09134797494b
Hugo Ribeiro <ribeirohugo@outlook.pt>
2025-04-10 11:41:23 +0100

    feat: add `jwt` authentication and middleware

---
+++ pkg/authentication/context/jwt.go
@@ -0,0 +1,51 @@
+package jwt
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/golang-jwt/jwt/v5"
+
+	"github.com/ribeirohugo/go_middlewares/pkg/authentication"
+)
+
+func (j JWT) parseClaims(ctx context.Context) (jwt.MapClaims, error) {
+	claims, ok := ctx.Value(j.Auth.ClaimsKey()).(*jwt.MapClaims)
+	if !ok {
+		return nil, fmt.Errorf("token not found in context")
+	}
+
+	return *claims, nil
+}
+
+// GetClaims allows to extract claims from context.
+func (j JWT) GetClaims(ctx context.Context) (authentication.Claims, error) {
+	claims, err := j.parseClaims(ctx)
+	if err != nil {
+		return authentication.Claims{}, err
+	}
+
+	userID, ok := claims["sub"]
+	if !ok {
+		return authentication.Claims{}, fmt.Errorf("user id wasn't found in claims")
+	}
+
+	role, ok := claims["role"]
+	if !ok {
+		return authentication.Claims{}, fmt.Errorf("role wasn't found in claims")
+	}
+
+	userRole := role.(string)
+
+	parsedClaims := authentication.Claims{
+		UserID: userID.(string),
+		Role:   userRole,
+	}
+
+	return parsedClaims, err
+}
+
+// Logout removes claims from the context, effectively logging the user out.
+func (j JWT) Logout(ctx context.Context) context.Context {
+	return context.WithValue(ctx, j.Auth.ClaimsKey(), nil)
+}
---
+++ pkg/authentication/context/model.go
@@ -0,0 +1,56 @@
+package jwt
+
+import (
+	"time"
+
+	"github.com/ribeirohugo/go_middlewares/pkg/authentication"
+)
+
+const (
+	expiredTokenMessage = "token has expired"
+	unauthorizedMessage = "unauthorized"
+)
+
+// JWT is a JWTMiddleware that holds authentication data and dependencies.
+//
+// adminRole is the maximum permission role, that allows everything by default.
+// claimsKey is the authentication key used by claims.
+// tokenSecret the secret key to verify the integrity and authenticity of the JWT
+// tokenMaxAge is the max duration of a token, in nanoseconds.
+// skipList is the list of endpoints that are ignored for JWT verification.
+// permissionsMap is the list of endpoints, associated to the allowed permission roles.
+type JWT struct {
+	AdminRole      string
+	ClaimsKey      any
+	PermissionsMap map[string][]string
+	SkipList       []string
+	TokenDuration  time.Duration
+	TokenSecret    string
+
+	Auth authentication.Auth
+}
+
+// New is a JWT middleware constructor.
+//
+// adminRole is the maximum permission role, that allows everything by default.
+// claimsKey is the authentication key used by claims.
+// tokenSecret the secret key to verify the integrity and authenticity of the JWT
+// tokenMaxAge is the max duration of a token, in nanoseconds.
+// skipList is the list of endpoints that are ignored for JWT verification.
+// permissionsMap is the list of endpoints, associated to the allowed permission roles.
+func New(
+	adminRole, tokenSecret string,
+	tokenMaxAge int,
+	claimsKey any,
+	skipList []string,
+	permissionsMap map[string][]string,
+) JWT {
+	return JWT{
+		AdminRole:      adminRole,
+		ClaimsKey:      claimsKey,
+		PermissionsMap: permissionsMap,
+		SkipList:       skipList,
+		TokenDuration:  time.Duration(tokenMaxAge),
+		TokenSecret:    tokenSecret,
+	}
+}
