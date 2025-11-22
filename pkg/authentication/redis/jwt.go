d19f4fdbb9a6e20f889a45c4c9af1432edf3a179
Hugo Ribeiro <ribeirohugo@outlook.pt>
2025-11-22 23:27:34 +0000

    fix: JWT Redis `Logout`

--- pkg/authentication/redis/jwt.go
+++ pkg/authentication/redis/jwt.go
@@ -3,7 +3,6 @@ package jwt
 import (
 	"context"
 	"fmt"
-	"time"
 
 	"github.com/golang-jwt/jwt/v5"
 	"github.com/redis/go-redis/v9"
@@ -32,56 +31,18 @@ func (j *JWT) GetClaims(ctx context.Context) (authentication.Claims, error) {
 	return claims, err
 }
 
-func (j *JWT) CheckLogin(ctx context.Context) (bool, error) {
-	claims, err := j.auth.ParseClaims(ctx)
-	if err != nil {
-		return false, err
-	}
-
-	if j.redis != nil {
-		_, err = j.redis.Get(ctx, claims.ID).Result()
-		if err != nil {
-			if err == redis.Nil {
-				return false, fmt.Errorf("key does not exist: %v", err)
-			}
-
-			return false, fmt.Errorf("redis error: %v", err)
-		}
-	}
-
-	token, err := jwt.ParseWithClaims(tokenString, &jwtClaims, func(token *jwt.Token) (any, error) {
-		if token.Method.Alg() != j.auth.SigningMethod.Alg() {
-			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
-		}
-
-		return []byte(j.auth.ClaimsKey), nil
-	})
-
-	if claims.ExpiresAt > 0 {
-		remainingTTL := time.Until(time.Unix(claims.ExpiresAt, 0))
-
-		if remainingTTL > 0 {
-			j.redis.Del(ctx, claims.ID)
-			return false, nil
-		}
-	}
-
-	return false, nil
-}
-
 // Logout removes claims from the context, effectively logging the user out.
-func (j *JWT) Logout(ctx context.Context) (context.Context, error) {
+func (j *JWT) Logout(ctx context.Context) context.Context {
 	if j.redis != nil {
-		claims, ok := ctx.Value(j.auth.ClaimsKey).(*authentication.Claims)
-		if ok && claims != nil {
-			err := j.redis.Del(ctx, claims.ID).Err()
-			if err != nil {
-				return ctx, fmt.Errorf("redis del error: %v", err)
-			}
+		claims, err := j.GetClaims(ctx)
+		if err != nil {
+			return ctx
 		}
+
+		j.redis.Del(ctx, claims.ID)
 	}
 
-	return context.WithValue(ctx, j.auth.ClaimsKey, nil), nil
+	return context.WithValue(ctx, j.auth.ClaimsKey, nil)
 }
 
 func (j *JWT) Login(ctx context.Context, subject, issuer, audience, role string) (string, error) {
